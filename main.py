a55baad8b130af281a07a6f2e055ca1270ff7833
BreakPoints <JackHooley7@gmail.com>
2017-01-03 22:45:02 +0000

    Upload work

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import datetime
 import jinja2
 import os
+import cloudstorage as gcs
 import webapp2
 
-from google.appengine.api import users
+from google.appengine.api import users, app_identity
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
+
+
+def get(self):
+    bucket_name = os.environ.get('BUCKET_NAME',
+                                 app_identity.get_default_gcs_bucket_name())
+
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.write('Demo GCS Application running from Version: '
+                        + os.environ['CURRENT_VERSION_ID'] + '\n')
+    self.response.write('Using bucket name: ' + bucket_name + '\n\n')
+
+
+def create_file(self, filename):
+    self.response.write('Creating file %s\n' % filename)
+
+    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+    gcs_file = gcs.open(filename,
+                        'w',
+                        content_type='text/plain',
+                        options={'x-goog-meta-foo': 'foo',
+                                 'x-goog-meta-bar': 'bar'},
+                        retry_params=write_retry_params)
+    gcs_file.write('abcde\n')
+    gcs_file.write('f'*1024*4 + '\n')
+    gcs_file.close()
+    self.tmp_filenames_to_clean_up.append(filename)
 
 
 class MainPage(webapp2.RequestHandler):
